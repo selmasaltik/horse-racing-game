74391ddc281c3273cbf2cfa1f3b3d679629ad615
selmasaltik <selmasaltiik@gmail.com>
2024-12-27 15:04:04 +0300

    Setup project structure

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>Horse Racing</div>
+  <div>
+    <div>Horse List</div>
+    <div>Horse Track</div>
+    <div>
+      <div>Program</div>
+      <div>Results</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
