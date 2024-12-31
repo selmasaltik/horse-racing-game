680694827354a593eab2a34afaa18626a822463a
selmasaltik <selmasaltiik@gmail.com>
2024-12-31 17:01:29 +0300

    Update App component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <script setup>
-  import { computed } from 'vue';
-  import { useStore } from 'vuex';
+ import { useApp } from './assets/composables/useApp.js';
 
-  import RaceControl from './components/RaceControl.vue';
-  import HorseList from './components/HorseList.vue';
-  import RaceTrack from './components/RaceTrack.vue';
-  import Program from './components/Program.vue';
-  import Results from './components/Results.vue';
+const { 
+  raceProgram, 
+  currentRace, 
+  raceInProgress, 
+  isProgramGenerated, 
+  raceResults 
+} = useApp();
 
-  const store = useStore();
+import RaceControl from './components/RaceControl.vue';
+import HorseList from './components/HorseList.vue';
+import RaceTrack from './components/RaceTrack.vue';
+import RaceProgram from './components/RaceProgram.vue';
+import RaceResults from './components/RaceResults.vue';
 
-  const raceProgram = computed(() => store.getters['race/raceProgram']);
-  const currentRace = computed(() => store.getters['race/currentRace']);
-  const raceInProgress = computed(() => store.getters['race/hasRaceStarted']);
-  const isProgramGenerated = computed(() => raceProgram.value.length > 0);
-  const raceResults = computed(() => store.getters['race/raceResults']); 
+import horseRaceStartImg from './assets/images/horse-racing-start.jpg';
+import horseRaceFinishImg from './assets/images/horse-racing-finish.jpg';
 </script>
 
 <template>
@@ -27,102 +29,25 @@
     </aside>
     <main class="main-content">
       <RaceTrack 
-        v-if="isProgramGenerated && currentRace < 6" 
+        v-if="isProgramGenerated && currentRace < 6"
+        :key="currentRace" 
       />
-      <p v-if="!isProgramGenerated" class="main-content__info">Please create a schedule.</p>
-      <p v-if="isProgramGenerated && currentRace === 6" class="main-content__info">Race is finished.</p> 
+      <div v-if="!isProgramGenerated" class="main-content__info">
+        <p>Please create a schedule.</p>
+        <img :src="horseRaceStartImg" alt="Horse Race Start" />
+      </div>
+      <div v-if="isProgramGenerated && currentRace === 6" class="main-content__info">
+        <p>Race is finished.</p>
+        <img :src="horseRaceFinishImg" alt="Horses Race Finish">
+      </div>
     </main>
     <aside class="right-sidebar" v-if="isProgramGenerated">
-      <Program :program="raceProgram" />
-      <Results :raceResults="raceResults" />
+      <RaceProgram :program="raceProgram" />
+      <RaceResults :raceResults="raceResults" />
     </aside>
   </div>
 </template>
 
 <style scoped lang="scss">
-  .app-container {
-    display: grid;
-    grid-template-areas:
-      "header"
-      "sidebar"
-      "main-content"
-      "right-sidebar";
-    grid-template-columns: 1fr;
-    grid-template-rows: auto 1fr 1fr 1fr;
-    height: 100vh;
-  }
-
-  .header {
-    grid-area: header;
-    min-height: 150px;
-    height: auto;
-    position: sticky;
-    top: 0;
-    z-index: 10;
-    background-color: #fff;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);  
-  }
-
-  .sidebar,
-  .main-content,
-  .right-sidebar {
-    padding: 20px;
-    background-color: #f7f7f7;
-  }
-
-  .sidebar {
-    grid-area: sidebar;
-  }
-
-  .main-content {
-    grid-area: main-content;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-
-    &__info {
-      display: flex;
-      align-items: flex-start;
-      height: 100%;
-      color: #555;
-      text-align: center;
-      font-size: 18px;
-      margin-top: 50px;
-    }
-  }
-
-  .right-sidebar {
-    grid-area: right-sidebar;
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-  }
-
-  .right-sidebar > * {
-    flex: 1; 
-  }
-
-  @media (min-width: 768px) {
-    .app-container {
-      grid-template-areas:
-        "header header header header"
-        "sidebar main-content right-sidebar right-sidebar";
-      grid-template-columns: 1fr 2fr 1fr 1fr;
-      grid-template-rows: auto 1fr;
-    }
-
-    .right-sidebar {
-      flex-direction: row;
-    }
-  }
-
-  @media (min-width: 1024px) {
-    .app-container {
-      grid-template-areas:
-        "header header header header"
-        "sidebar main-content right-sidebar right-sidebar";
-      grid-template-columns: 1fr 2fr 1fr 1fr;
-      grid-template-rows: auto 1fr;
-    }
-  }
+  @use './assets/styles/app.scss';
 </style>
