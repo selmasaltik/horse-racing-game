15c929a85875b6ec10d9486ee50bb46fdbaf0b8e
selmasaltik <selmasaltiik@gmail.com>
2024-12-31 17:01:07 +0300

    Update styles structure and added images

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
+import './assets/styles/main.scss'
 
 import App from './App.vue';
-import store from './store';
+import store from './store/use-main';
   
 const app = createApp(App);
 app.use(store);
